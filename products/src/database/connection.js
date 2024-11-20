9b30c3be743efec9baba2bbfd3ef109290f8e5b9
Danish Yousuf Mir <danishmir9991@gmail.com>
2024-11-20 23:15:04 +0530

    updated

--- products/src/database/connection.js
+++ products/src/database/connection.js
@@ -12,7 +12,6 @@ module.exports = async() => {
         console.log('Db Connected');
         
     } catch (error) {
-        console.log('Error ============')
         console.log(error);
         process.exit(1);
     }
